e7bef369eefbf7b16df1d4bba9d8e71b9f5107a3
Christian Lück <christian@lueck.tv>
2013-04-06 12:22:55 +0200

    Pass SelectPoller to each Socket

--- Sockets/Factory.php
+++ Sockets/Factory.php
@@ -7,7 +7,8 @@ use \Exception;
 
 class Factory
 {
-    private $pollInterval = 0.01;
+    private $loop;
+    private $poller = null;
 
     public function __construct(LoopInterface $loop)
     {
@@ -27,12 +28,20 @@ class Factory
         return $this->create(AF_INET6, SOCK_DGRAM, SOL_UDP);
     }
 
+    public function getPoller()
+    {
+        if ($this->poller === null) {
+            $this->poller = new SelectPoller($this->loop);
+        }
+        return $this->poller;
+    }
+
     private function create($domain, $type, $protocol)
     {
         $sock = socket_create($domain, $type, $protocol);
         if ($sock === false) {
             throw new Exception('Unable to create socket');
         }
-        return new Socket($sock, $this->pollInterval, $this->loop);
+        return new Socket($sock, $this->getPoller());
     }
 }
