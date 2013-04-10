6ae480f965ce31fba4db495c141fe9ba19f99ee2
Christian Lück <christian@lueck.tv>
2013-04-10 11:28:26 +0200

    First sketch to use Socket\Raw\Factory

--- Sockets/Factory.php
+++ Sockets/Factory.php
@@ -2,46 +2,49 @@
 
 namespace Sockets;
 
+use React\Promise\When;
+
+use React\Promise\Deferred;
 use React\EventLoop\LoopInterface;
+use Socket\Raw\Factory as RawFactory;
 use \Exception;
 
 class Factory
 {
     private $loop;
+    private $rawFactory;
     private $poller = null;
 
     public function __construct(LoopInterface $loop)
     {
-        if (!function_exists('socket_create')) {
-            throw new Exception('Sockets extension not loaded');
-        }
         $this->loop = $loop;
+        $this->rawFactory = new RawFactory();
     }
 
     public function createUdp4()
     {
-        return $this->create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        return new Datagram($this->rawFactory->createUdp4(), $this->getPoller());
     }
 
     public function createUdp6()
     {
-        return $this->create(AF_INET6, SOCK_DGRAM, SOL_UDP);
+        return new Datagram($this->rawFactory->createUdp6(), $this->getPoller());
     }
 
+    public function createUdg()
+    {
+        return new Datagram($this->rawFactory->createUdg(), $this->getPoller());
+    }
+
+    /**
+     *
+     * @return SelectPoller
+     */
     public function getPoller()
     {
         if ($this->poller === null) {
             $this->poller = new SelectPoller($this->loop);
         }
         return $this->poller;
-    }
-
-    private function create($domain, $type, $protocol)
-    {
-        $sock = socket_create($domain, $type, $protocol);
-        if ($sock === false) {
-            throw new Exception('Unable to create socket');
-        }
-        return new Socket($sock, $this->getPoller());
     }
 }
