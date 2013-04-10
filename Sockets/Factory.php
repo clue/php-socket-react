5f7d2866705e0ff499b4aedfb47c0e54f77d23d7
Christian Lück <christian@lueck.tv>
2013-04-10 11:41:17 +0200

    Use Promises to prepare for async DNS resolution

--- Sockets/Factory.php
+++ Sockets/Factory.php
@@ -25,34 +25,40 @@ class Factory
      * create stream client socket connected to given address
      *
      * @param string $address
-     * @return \Sockets\Stream
+     * @return PromiseInterface to return a \Sockets\Stream
      * @throws Exception on error
      * @uses RawFactory::createClient()
      */
     public function createClient($address)
     {
-        $socket = $this->rawFactory->createClient($address);
-        if ($socket->getType() !== SOCK_STREAM) {
-            $socket->close();
-            throw new Exception('Not a stream address scheme');
-        }
-        return new Stream($socket, $this->getPoller());
+        $factory = $this->rawFactory;
+        $that = $this;
+        return $this->resolve($address)->then(function ($address) use ($factory, $that) {
+            $socket = $factory->createClient($address);
+            if ($socket->getType() !== SOCK_STREAM) {
+                $socket->close();
+                throw new Exception('Not a stream address scheme');
+            }
+            return new Stream($socket, $that->getPoller());
+        });
     }
 
     /**
      * create stream server socket bound to and listening on the given address for incomming stream client connections
      *
      * @param string $address
-     * @return \Sockets\Server
+     * @return PromiseInterface to return a \Sockets\Server
      * @throws Exception on error
      * @uses Server::listenAddress()
      */
     public function createServer($address)
     {
-        $server = new Server($this->getPoller(), $this->rawFactory);
-        $server->listenAddress($address);
+        return $this->resolve($address)->then(function ($address) {
+            $server = new Server($this->getPoller(), $this->rawFactory);
+            $server->listenAddress($address);
 
-        return $server;
+            return $server;
+        });
     }
 
     public function createUdp4()
@@ -80,5 +86,21 @@ class Factory
             $this->poller = new SelectPoller($this->loop);
         }
         return $this->poller;
+    }
+
+    /**
+     * resolve given address via DNS if applicable
+     *
+     * Letting host names pass through will not break things, but it
+     * requires a blocking resolution afterwards. So make sure to try to
+     * resolve hostnames here.
+     *
+     * @param string $address
+     * @return PromiseInterface
+     * @todo use Resolver to perform async resolving
+     */
+    private function resolve($address)
+    {
+        return When::resolve($address);
     }
 }
