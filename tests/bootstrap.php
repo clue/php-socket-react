580f3f950e8c1644adb8a07fea1a805102a83a75
Christian Lück <christian@lueck.tv>
2013-05-10 00:26:48 +0200

    Add callback tests from reactphp/react

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,39 @@
 
 class TestCase extends PHPUnit_Framework_TestCase
 {
+    protected function expectCallableOnce()
+    {
+        $mock = $this->createCallableMock();
+        $mock
+            ->expects($this->once())
+            ->method('__invoke');
     
+        return $mock;
+    }
+    
+    protected function expectCallableNever()
+    {
+        $mock = $this->createCallableMock();
+        $mock
+            ->expects($this->never())
+            ->method('__invoke');
+    
+        return $mock;
+    }
+    
+    /**
+     * @link https://github.com/reactphp/react/blob/master/tests/React/Tests/Socket/TestCase.php (taken from reactphp/react)
+     */
+    protected function createCallableMock()
+    {
+        return $this->getMock('CallableStub');
+    }
 }
+
+class CallableStub
+{
+    public function __invoke()
+    {
+    }
+}
+
