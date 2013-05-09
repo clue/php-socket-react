d3190c117b87cda251fc3c32591836cb8068c093
Christian Lück <christian@lueck.tv>
2013-05-10 01:02:05 +0200

    Test `createClient()` and `createServer()`

--- tests/Datagram/FactoryTest.php
+++ tests/Datagram/FactoryTest.php
@@ -9,10 +9,12 @@ class DatagramFactoryTest extends TestCase{
      */
     protected $factory;
 
+    protected $loop;
+
     public function setUp()
     {
-        $loop = React\EventLoop\Factory::create();
-        $this->factory = new Factory($loop);
+        $this->loop = React\EventLoop\Factory::create();
+        $this->factory = new Factory($this->loop);
     }
 
     public function testSupportsIpv6()
@@ -35,34 +37,134 @@ class DatagramFactoryTest extends TestCase{
     {
         $this->assertInstanceOf('Socket\React\Datagram\Factory', $this->factory);
     }
-    
+
+    public function testCreateClientUdp4()
+    {
+        $promise = $this->factory->createClient('udp://127.0.0.1:53');
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $promise->then($this->expectCallableOnceParameter('Socket\React\Datagram\Datagram'), $this->expectCallableNever());
+
+        $this->loop->tick();
+    }
+
+    public function testCreateClientSchemelessUdp4()
+    {
+        $promise = $this->factory->createClient('127.0.0.1:53');
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $promise->then($this->expectCallableOnceParameter('Socket\React\Datagram\Datagram'), $this->expectCallableNever());
+
+        $this->loop->tick();
+    }
+
+    /**
+     * @depends testSupportsIpv6
+     */
+    public function testCreateClientSchemelessUdp6()
+    {
+        $promise = $this->factory->createClient('[::1]:53');
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $promise->then($this->expectCallableOnceParameter('Socket\React\Datagram\Datagram'), $this->expectCallableNever());
+
+        $this->loop->tick();
+    }
+
+    /**
+     * creating a TCP socket fails because it is NOT a datagram socket
+     */
+    public function testCreateClientFailTcp()
+    {
+        $promise = $this->factory->createClient('tcp://www.google.com:80');
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $promise->then($this->expectCallableNever(), $this->expectCallableOnceParameter('Exception'));
+
+        $this->loop->tick();
+    }
+
+    public function testCreateServerUdp4()
+    {
+        $promise = $this->factory->createServer('udp://127.0.0.1:0');
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $promise->then($this->expectCallableOnceParameter('Socket\React\Datagram\Datagram'), $this->expectCallableNever());
+
+        $this->loop->tick();
+    }
+
+    public function testCreateServerSchemelessUdp4()
+    {
+        $promise = $this->factory->createServer('127.0.0.1:0');
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $promise->then($this->expectCallableOnceParameter('Socket\React\Datagram\Datagram'), $this->expectCallableNever());
+
+        $this->loop->tick();
+    }
+
+    /**
+     * @depends testSupportsIpv6
+     */
+    public function testCreateServerSchemelessUdp6()
+    {
+        $promise = $this->factory->createServer('[::1]:0');
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $promise->then($this->expectCallableOnceParameter('Socket\React\Datagram\Datagram'), $this->expectCallableNever());
+
+        $this->loop->tick();
+    }
+
+    /**
+     * creating a TCP socket fails because it is NOT a datagram socket
+     */
+    public function testCreateServerFailTcp()
+    {
+        $promise = $this->factory->createServer('tcp://127.0.0.1:0');
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $promise->then($this->expectCallableNever(), $this->expectCallableOnceParameter('Exception'));
+
+        $this->loop->tick();
+    }
+
     public function testCreateUdp4()
     {
         $socket = $this->factory->createUdp4();
-        
+
         $this->assertInstanceOf('Socket\React\Datagram\Datagram', $socket);
     }
-    
+
     /**
      * @depends testSupportsIpv6
      */
     public function testCreateUdp6()
     {
         $socket = $this->factory->createUdp6();
-    
+
         $this->assertInstanceOf('Socket\React\Datagram\Datagram', $socket);
     }
-    
+
     /**
      * @depends testSupportsUnix
      */
     public function testCreateUdg()
     {
         $socket = $this->factory->createUdg();
-    
+
         $this->assertInstanceOf('Socket\React\Datagram\Datagram', $socket);
     }
-    
+
     public function testCreateIcmp4()
     {
         try {
@@ -75,10 +177,10 @@ class DatagramFactoryTest extends TestCase{
             }
             throw $e;
         }
-    
+
         $this->assertInstanceOf('Socket\React\Datagram\Datagram', $socket);
     }
-    
+
     /**
      * @depends testSupportsIpv6
      */
@@ -94,7 +196,7 @@ class DatagramFactoryTest extends TestCase{
             }
             throw $e;
         }
-    
+
         $this->assertInstanceOf('Socket\React\Datagram\Datagram', $socket);
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,20 +10,31 @@ class TestCase extends PHPUnit_Framework_TestCase
         $mock
             ->expects($this->once())
             ->method('__invoke');
-    
+
         return $mock;
     }
-    
+
     protected function expectCallableNever()
     {
         $mock = $this->createCallableMock();
         $mock
             ->expects($this->never())
             ->method('__invoke');
-    
+
         return $mock;
     }
-    
+
+    protected function expectCallableOnceParameter($type)
+    {
+        $mock = $this->createCallableMock();
+        $mock
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($this->isInstanceOf($type));
+
+        return $mock;
+    }
+
     /**
      * @link https://github.com/reactphp/react/blob/master/tests/React/Tests/Socket/TestCase.php (taken from reactphp/react)
      */
