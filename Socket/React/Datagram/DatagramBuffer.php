2e40a051014d5b23e8bd6d0117d5283e0d719aa8
Christian Lück <christian@lueck.tv>
2013-06-25 00:11:42 +0200

    Reuse & extend Socket and Buffer from clue/datagram

--- Socket/React/Datagram/DatagramBuffer.php
+++ Socket/React/Datagram/DatagramBuffer.php
@@ -2,57 +2,34 @@
 
 namespace Socket\React\Datagram;
 
-use React\EventLoop\LoopInterface;
-use Socket\Raw\Socket as RawSocket;
+use Datagram\BufferAbstract;
 
-class DatagramBuffer
+class DatagramBuffer extends BufferAbstract
 {
-    private $socket;
-    private $loop;
+    private $listening = false;
 
-    private $outgoing = array();
-    private $outgoingLength = 0;
-
-    private $softLimit = 65536;
-
-    public function __construct(RawSocket $socket, LoopInterface $loop)
+    protected function handleWrite($data, $remoteAddress)
     {
-        $this->socket = $socket;
-        $this->loop   = $loop;
-    }
-
-    public function send($data, $remote = null)
-    {
-        $this->outgoing []= array($data, $remote);
-        $this->outgoingLength += strlen($data);
-
-        $this->loop->addWriteStream($this->socket->getResource(), array($this, 'handleWrite'));
-        // $this->poller->notify();
-
-        return ($this->outgoingLength < $this->softLimit);
-    }
-
-    public function handleWrite()
-    {
-        list($data, $remote) = array_shift($this->outgoing);
-        $this->outgoingLength -= strlen($data);
-
-        if ($remote === null) {
+        if ($remoteAddress === null) {
             $this->socket->send($data, 0);
         } else {
-            $this->socket->sendTo($data, 0, $remote);
-        }
-
-        if (!$this->outgoing) {
-            $this->loop->removeWriteStream($this->socket->getResource());
+            $this->socket->sendTo($data, 0, $remoteAddress);
         }
     }
 
-    public function close()
+    protected function pause()
     {
-        $this->loop->removeWriteStream($this->socket->getResource());
+        if ($this->listening) {
+            $this->loop->removeWriteStream($this->socket->getResource());
+            $this->listening = false;
+        }
+    }
 
-        $this->outgoing = array();
-        $this->outgoingLength = 0;
+    protected function resume()
+    {
+        if (!$this->listening) {
+            $this->loop->addWriteStream($this->socket->getResource(), array($this, 'onWritable'));
+            $this->listening = true;
+        }
     }
 }
--- Socket/React/Datagram/Socket.php
+++ Socket/React/Datagram/Socket.php
@@ -2,32 +2,15 @@
 
 namespace Socket\React\Datagram;
 
-use React\EventLoop\LoopInterface;
-use Evenement\EventEmitter;
-use Socket\Raw\Socket as RawSocket;
-use Datagram\SocketInterface;
-use \Exception;
+use Datagram\Socket as DatagramSocket;
+use Socket\React\Datagram\DatagramBuffer;
+use Exception;
 
-class Socket extends EventEmitter implements SocketInterface
+class Socket extends DatagramSocket
 {
-    private $socket;
-    private $loop;
-    private $buffer;
-    private $bufferSize = 65536;
-
-    public function __construct(RawSocket $socket, LoopInterface $loop)
-    {
-        $this->socket = $socket;
-        $this->loop = $loop;
-
-        $this->buffer = new DatagramBuffer($socket, $loop);
-
-        $this->resume();
-    }
-
     public function resume()
     {
-        $this->loop->addReadStream($this->socket->getResource(), array($this, 'handleRead'));
+        $this->loop->addReadStream($this->socket->getResource(), array($this, 'onReceive'));
     }
 
     public function pause()
@@ -35,47 +18,23 @@ class Socket extends EventEmitter implements SocketInterface
         $this->loop->removeReadStream($this->socket->getResource());
     }
 
-    /**
-     * send given $data as a datagram message to given $remote address or connect()ed target
-     *
-     * @param string      $data   datagram message to be sent
-     * @param string|null $remote remote/peer address to send message to. can be null if your client socket is connect()ed
-     * @return boolean
-     * @uses DatagramBuffer::send()
-     * @see self::connect()
-     */
-    public function send($data, $remote = null)
+    protected function createBuffer()
     {
-        return $this->buffer->send($data, $remote);
+        return new DatagramBuffer($this->loop, $this->socket);
     }
 
-    public function handleRead()
+    protected function handleReceive(&$remote)
     {
-        try {
-            $data = $this->socket->recvFrom($this->bufferSize, 0, $remote);
-        }
-        catch (Exception $e) {
-            $this->emit('error', array($e, $this));
-            return;
-        }
-
-        $this->emit('message', array($data, $remote, $this));
+        return $this->socket->recvFrom($this->bufferSize, 0, $remote);
     }
 
-    public function close()
+    protected function handleClose()
     {
-        $this->emit('close', array($this));
-
-        $this->pause();
-        $this->buffer->close();
-
         try {
             $this->socket->shutdown();
         }
         catch (Exception $ignore) {
         }
         $this->socket->close();
-
-        $this->removeAllListeners();
     }
 }
